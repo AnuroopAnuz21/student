f778141990bf87f63f0d173781bbee029035111c
AnuroopAnuz21 <anuroopprasad731@gmail.com>
2022-07-02 14:07:50 +0530

    complete

---
+++ src/app/anu/anu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anu',
+  templateUrl: './anu.component.html',
+  styleUrls: ['./anu.component.css']
+})
+export class AnuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnuComponent } from './anu/anu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AnuComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
